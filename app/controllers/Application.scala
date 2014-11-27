2899ca7d148c11817c7992597b98003093696214
rajeshponnala <rajesh.ponnala@technoidentiy.com>
2014-11-27 16:21:59 +0530

    comment database related code

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -11,14 +11,14 @@ object Application extends Controller {
   def index = Action {
      
   var x=""
-  Database.forURL("jdbc:mysql://localhost:3306/inventory", driver = "scala.slick.driver.MySQLDriver",user="root",password="root") withSession {
-  implicit session =>
+  //Database.forURL("jdbc:mysql://localhost:3306/inventory", driver = "scala.slick.driver.MySQLDriver",user="root",password="root") withSession {
+ // implicit session =>
   // <- write queries here
-  var list =for(product <- Products) yield  product
-  list foreach { product =>
-     x+=","+product.ean
-  }
-  Ok(views.html.index(x))
+ // var list =for(product <- Products) yield  product
+ // list foreach { product =>
+   //  x+=","+product.ean
+ // }
+  Ok(views.html.index())
   }
  }
 }
