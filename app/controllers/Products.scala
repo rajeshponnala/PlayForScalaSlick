b009077462d30e9e398fdbd4bf682c106e32093e
rajeshponnala <rajesh.ponnala@technoidentiy.com>
2014-11-28 16:59:46 +0530

    add  implicit write json formatter for product

--- app/controllers/Products.scala
+++ app/controllers/Products.scala
@@ -2,19 +2,32 @@ package controllers
 
 import play.api._
 import play.api.mvc.{Action, Controller}
-import play.api.libs.json.Json
+import play.api.libs.json._
 import Tables._
 import Tables.profile.simple._
+import play.api.libs.functional.syntax._
 
 
 object Prod extends Controller {
+  //implicit val locationWrites: Writes[ProductsRow] = ((JsPath \ "ean").write[Long]and (JsPath \ "name").write[String]and  (JsPath \ "description").write[String])(unlift(ProductsRow.unapply))
+ implicit val productWrites = new Writes[ProductsRow] {
+  def writes(p: ProductsRow) = Json.obj(
+    "ean" -> p.ean,
+    "name" -> p.name,
+    "description"->p.description
+  )
+}
+   
    def list = Action{
   	 var data= Database.forURL("jdbc:mysql://localhost:3306/inventory", driver = "scala.slick.driver.MySQLDriver",user="root",password="root") withSession {
        implicit session =>
-      (for(product <- Products) yield  product.ean).list
-      
+      //(for(product <- Products) yield  product.ean).list
+       Products.list
     }
     Ok(Json.toJson(data))
+    
   }
+
+  
 }
 
